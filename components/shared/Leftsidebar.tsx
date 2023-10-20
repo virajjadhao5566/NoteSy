bbdf076cce54a51b2ed784fdac893ec125cbfe45
Viraj <virajjadhao85@gmail.com>
2023-10-20 18:58:35 +0530

    sidebar done

--- components/shared/Leftsidebar.tsx
+++ components/shared/Leftsidebar.tsx
@@ -1,4 +1,52 @@
-function Leftsidebar(){
-    return <h1>Leftsidebar</h1>
+"use client"
+import { sidebarLinks } from "@/constants"
+import { SignOutButton, SignedIn } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname, useRouter } from "next/navigation"
+
+function Leftsidebar() {
+    const router = useRouter()
+    const pathName = usePathname()
+    return (
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+                {sidebarLinks.map((link) => {
+                    const isActive = (pathName.includes(link.route) && link.route.length > 1) || pathName === link.route;
+                    return (
+                        <Link
+                            href={link.route}
+                            key={link.label}
+                            className={`leftsidebar_link ${isActive && 'bg-gray-800'}`}
+                        >
+                            <Image
+                                src={link.imgURL}
+                                alt={link.label}
+                                width={24}
+                                height={24}
+                            />
+                            <p className="text-light-1 max-lg:hidden">{link.label}</p>
+                        </Link>
+                    )
+                }
+                )}
+            </div>
+            <div className="mt-10 px-6">
+                <SignedIn>
+                    <SignOutButton signOutCallback={()=>router.push('/sign-in')}>
+                        <div className="flex cursor-pointer gap-4 p-4">
+                            <Image
+                                src={'/assets/logout.svg'}
+                                alt="logout"
+                                width={24}
+                                height={24}
+                            />
+                            <p className="text-light-2 max-lg:hidden">LogOut</p>
+                        </div>
+                    </SignOutButton>
+                </SignedIn>
+            </div>
+        </section>
+    )
 }
 export default Leftsidebar
