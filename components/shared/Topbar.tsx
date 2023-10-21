68f28ee7e2823fab13365a68f382a7ca8f6e0442
Viraj <virajjadhao85@gmail.com>
2023-10-21 11:30:53 +0530

    layout done

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,4 +1,5 @@
 import { OrganizationSwitcher, SignIn, SignOutButton, SignedIn } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 import Image from "next/image"
 import Link from "next/link"
 import { useState } from "react"
@@ -27,6 +28,7 @@ function Topbar(){
                 </div>
                 <OrganizationSwitcher
                     appearance={{
+                        baseTheme:dark,
                         elements:{
                             organizationSwitcherTrigger:"py-2 px-4"
                         }
