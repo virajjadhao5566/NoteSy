81d0259e12205555d8521e1aca8bf904629ab35f
Viraj <virajjadhao85@gmail.com>
2023-10-21 15:33:36 +0530

    shadcn configured

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,25 @@
+import AccountProfile from "@/components/forms/accountProfile";
+import { currentUser } from "@clerk/nextjs";
 async function Page(){
+    const user = await currentUser()
+    const userData = {
+        id:user?.id,
+        objectId:userInfo?._id,
+        username:userInfo?.username || user?.username,
+        name:userInfo?.name || user?.firstName || "",
+        bio:userInfo?.bio || "",
+        image: userInfo?.image || user?.imageUrl,
+    }
     return(
-        <main>
+        <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
             <h1 className="head-text">Onboarding</h1>
+            <p className="text-light-1">Complete your profile to use Notesy</p>
+            <section className="mt-9 bg-dark-2 p-10">
+                <AccountProfile 
+                    user = {userData}
+                    btnTitle = "Continue"
+                />
+            </section>
         </main>
     )
 }
